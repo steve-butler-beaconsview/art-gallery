330bf5dfaefe1cbc78768f86ab7d29570877f940
Steve Butler <steve.butler@beaconsviewtech.com>
2025-06-11 18:59:05 -0400

    PageNav component
    - added new component to offer page navigation interactive elements
      - arguments:
        - pageNumber: current index of pagination (1-based)
        - totalPageCount: total number of pagination pages
        - prevPageAvailable: indicates whether the user should be able to navigate to the previous page
        - nextPageAvailable: indicates whether the user should be able to navigate to the next page
      - component has previous page and next page buttons, and shows current page number and total number of pages in between the buttons
    - added Material Icons via google font stylesheet
    - in gallery model, save pagination properties from API response for passing into PageNav

--- app/routes/gallery.js
+++ app/routes/gallery.js
@@ -6,12 +6,23 @@ export default class GalleryRoute extends Route {
     try {
       const gridColumns = 5;
       const gridRows = 5;
+      const currentPageNumber = 1;
       const maxRetrievalSize = gridColumns * gridRows;
-      const { data: { data } } = await axios.get(`https://api.artic.edu/api/v1/artworks?page=1&limit=${maxRetrievalSize}&fields=id,image_id,width,height`);
+      const pageResponse = await axios.get(`https://api.artic.edu/api/v1/artworks?page=${currentPageNumber}&limit=${maxRetrievalSize}&fields=id,image_id,width,height`);
+      const { data: { data } } = pageResponse;
+      const {
+        total_pages: totalPageCount,
+        next_url: nextPageUrl,
+        prev_url: prevPageUrl,
+      } = pageResponse.data.pagination;
       const imageUrls = data.map(({ image_id }) => image_id).map(id => `https://www.artic.edu/iiif/2/${id}/full/200,/0/default.jpg`)
       const modelData = {
         gridRows,
         gridColumns,
+        currentPageNumber,
+        totalPageCount,
+        nextPageAvailable: !!nextPageUrl,
+        prevPageAvailable: !!prevPageUrl,
         imageUrls,
       }
       return modelData;
