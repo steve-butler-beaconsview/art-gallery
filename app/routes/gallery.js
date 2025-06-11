928d5b7a494c9bd747407d1527a7a468a337f8fc
Steve Butler <steve.butler@beaconsviewtech.com>
2025-06-11 07:30:54 -0400

    Simple retrieval and rendering of first page of artworks
    - added axios HTTP client
    - arrange model in rows and columns for ease of rendering in template
    - display artworks in static grid with correct aspect ratio

--- app/routes/gallery.js
+++ app/routes/gallery.js
@@ -1,3 +1,26 @@
 import Route from '@ember/routing/route';
+import axios from 'axios';
 
-export default class GalleryRoute extends Route {}
+export default class GalleryRoute extends Route {
+  async model () {
+    try {
+      const matrixColumns = 5;
+      const matrixRows = 5;
+      const maxRetrievalSize = matrixColumns * matrixRows;
+      const { data: { data } } = await axios.get(`https://api.artic.edu/api/v1/artworks?page=1&limit=${maxRetrievalSize}&fields=id,image_id,width,height`);
+      const modelData = data.map(({ image_id }) => image_id).reduce((acc, id, ndx, allIds) => {
+        if (ndx % matrixColumns === 0) {
+          return [
+            ...acc,
+            allIds.slice(ndx, ndx + matrixColumns)
+          ];
+        }
+        return acc;
+      }, []);
+      debugger;
+      return modelData;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
