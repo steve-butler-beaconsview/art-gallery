ac878b1996ceed864db4d8df65902aaf39f83fcf
Steve Butler <steve.butler@beaconsviewtech.com>
2025-06-13 08:54:06 -0400

    Artwork viewed indicator icons overlaid on gallery thumbnail

--- app/components/image-tile.js
+++ app/components/image-tile.js
@@ -3,9 +3,13 @@ import { service } from '@ember/service';
 
 export default class ImageTileComponent extends Component {
   @service('favourites') favouritesService;
+  @service('view-tracker') viewTrackerService;
 
   get isFavourite () {
     return this.favouritesService.isFavourite(this.args.imageId);
+  }
+  get wasViewed () {
+    return this.viewTrackerService.wasViewed(this.args.imageId);
   }
 }
 
